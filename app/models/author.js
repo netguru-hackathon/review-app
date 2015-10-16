dc6b386bf1b0c27c64ce90982438d3db62d67c6c
Marcin Brzeziński <cintrzyk@gmail.com>
2015-10-16 14:43:00 +0200

    author & user models

--- app/models/author.js
+++ app/models/author.js
@@ -1,4 +1,9 @@
 import DS from 'ember-data';
 
+const attr = DS.attr;
+
 export default DS.Model.extend({
+  username: attr('string'),
+  name:     attr('string'),
+  imageUrl: attr('string')
 });
--- app/models/user.js
+++ app/models/user.js
@@ -1,4 +1,28 @@
 import DS from 'ember-data';
 
+const attr = DS.attr;
+const hasMany = DS.hasMany;
+const belongsTo = DS.belongsTo;
+
 export default DS.Model.extend({
+  imageUrl: attr('string'),
+  name: attr('string'),
+  nickname: attr('string'),
+  email: attr('string'),
+  admin: attr('boolean'),
+  person: belongsTo('author'),
+  token: attr('string'),
+  roles: hasMany('role'),
+  role: attr('string'),
+  projectIds: attr('object'),
+
+  isAdmin: Ember.computed('admin', function() {
+    return this.get('admin') === true;
+  }),
+
+  allRoles: Ember.computed('roles', function() {
+    return this.get('roles').getEach('name');
+  }),
+
+  userRole: Ember.computed.uniq('allRoles')
 });
