f707cd69f019ccd91dbf414e8900ee9aa328f3ff
Marcin Brzeziński <cintrzyk@gmail.com>
2015-10-16 14:43:11 +0200

    remove useless comment

--- app/pods/project/route.js
+++ app/pods/project/route.js
@@ -3,10 +3,5 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model: function(params) {
     return this.store.queryRecord('project', { name: params.name} );
-  },
-
-  // serialize: function(model) {
-  //   // this will make the URL `/posts/foo-post`
-  //   return { project_slug: model.get('name') };
-  // }
+  }
 });
