1789b2262ae134fe7e1f7a26189a5db91ed87c04
Marcin Brzeziński <cintrzyk@gmail.com>
2015-10-16 18:34:11 +0200

    search projects

--- app/pods/components/main-sidebar/component.js
+++ app/pods/components/main-sidebar/component.js
@@ -1,5 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  classNames: ['pane', 'pane-sm', 'sidebar']
+  classNames: ['pane', 'pane-sm', 'sidebar'],
+  term: '',
+  filteredProjects: Ember.computed('term', 'projects', function() {
+    let term = this.get('term');
+
+    if (Ember.isEmpty(term)) {
+      return this.get('projects');
+    } else {
+      return this.get('projects').filter(function(item, index, enumerable) {
+        return item.get('name').startsWith(term);
+      });
+    }
+  })
 });
